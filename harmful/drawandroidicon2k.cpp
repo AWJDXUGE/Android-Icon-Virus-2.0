3edb8d666c5e0d5f5733ec9437b603f4fe684e92
AWJDXUGE <1pg_rrowpe00ba@dingtalk.com>
2024-08-13 12:14:16 +0800

    Add files via upload

---
+++ harmful/drawandroidicon2k.cpp
@@ -0,0 +1,28 @@
+﻿#include<tchar.h>
+#include<windows.h>
+#include<ctime>
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+using namespace std;
+
+DWORD WINAPI drawandroidicon(LPVOID lpParam) {
+	HWND hwnd = GetDesktopWindow();
+	HINSTANCE ANDROIDICON = LoadLibrary(_T("ANDROIDICON2K.dll"));
+	HDC hdc = GetWindowDC(hwnd);
+	srand(time(0));
+	Sleep(1000);
+	while (1) {
+		for (int x = 0; x <= 3840; x += 32) {
+			for (int y = 0; y <= 2160; y += 32) {
+				DrawIcon(hdc, x, y, LoadIcon(ANDROIDICON, MAKEINTRESOURCE(rand() % 256)));
+			}
+		}
+		Sleep(1000);
+	}
+	while (1);
+}
+
+int main() {
+	HANDLE thread1 = CreateThread(0, 0, drawandroidicon, 0, 0, 0);
+	Sleep(310000);
+	return 0;
+}
---
+++ harmful/drawxugeicon2k.cpp
@@ -0,0 +1,24 @@
+﻿#include<tchar.h>
+#include<windows.h>
+#include<ctime>
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+using namespace std;
+
+DWORD WINAPI drawxugeicon(LPVOID lpParam) {
+	HWND hwnd = GetDesktopWindow();
+	HINSTANCE XUGEICON = LoadLibrary(_T("XUGEICON2K.dll"));
+	HDC hdc = GetWindowDC(hwnd);
+	POINT point;
+	while (1)
+	{
+		GetCursorPos(&point);
+		DrawIcon(hdc, point.x + 0, point.y + 0, LoadIcon(XUGEICON, MAKEINTRESOURCE(rand() % 256)));
+
+	}Sleep(1);
+}
+
+int main() {
+	HANDLE thread1 = CreateThread(0, 0, drawxugeicon, 0, 0, 0);
+	Sleep(310000);
+	return 0;
+}
